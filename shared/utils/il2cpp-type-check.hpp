85448c1468095343710af7a9877e8e94e68fc184
Adam ? <adamznow@gmail.com>
2020-11-12 21:54:56 -0800

    classof, csTypeOf, and fix for generics

--- shared/utils/il2cpp-type-check.hpp
+++ shared/utils/il2cpp-type-check.hpp
@@ -26,13 +26,13 @@ constexpr bool has_get = std::experimental::is_detected_v<get_t, T>;
 #ifndef classof
 // Returns the Il2CppClass* of the provided type T.
 // Should be a pointer type if it is a reference type, otherwise it should be a value type.
-#define classof(T) ::il2cpp_utils::il2cpp_type_check::il2cpp_no_arg_class<T>::get()
+#define classof(...) (::il2cpp_utils::il2cpp_type_check::il2cpp_no_arg_class<__VA_ARGS__>::get())
 #endif
 
-#ifndef typeof
+#ifndef csTypeOf
 // Returns the Il2CppReflectionType* of the provided type T.
 // Should be a pointer type if it is a reference type, otherwise it should be a value type.
-#define typeof(T) ::il2cpp_utils::GetSystemType(classof(T))
+#define csTypeOf(...) (::il2cpp_utils::GetSystemType(classof(__VA_ARGS__)))
 #endif
 
 namespace il2cpp_utils {
